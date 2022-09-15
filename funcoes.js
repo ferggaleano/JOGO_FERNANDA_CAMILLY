0fc29c32f97dae178ea1e710686763b57a8e8255
camyx-7 <camillylimabarrios@gmail.com>
2022-09-14 21:14:19 -0400

    começo do js e alterações no index

---
+++ funcoes.js
@@ -0,0 +1,12 @@
+let personagem = document.querySelector('#personagem')
+let quadrado = document.querySelector('#quadrado')
+
+function pular(){
+    if(personagem.classList != 'animar'){
+        personagem.classList.add('animar')
+    }
+
+    setTimeout(function(){
+        personagem.classList.remove('animar')
+    }, 500)
+}
